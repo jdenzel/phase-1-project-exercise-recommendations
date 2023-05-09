207fdc612e74ba366f82d9423e647670942e2f38
Denzel <jdenzelmab@gmail.com>
2023-05-08 22:22:34 -0400

    changed color theme of the webpage

--- main.js
+++ main.js
@@ -28,23 +28,19 @@ const box = document.getElementsByClassName('box')
     box.style.borderColor = 'white';
     console.log("you are hovering")
 })
+
 function createInfoBox(excercise){
     let infoBox = document.createElement("div")
     infoBox.classList.add("box")
-
     let name = document.createElement("h2")
     name.textContent = excercise.name
-
     let gif = document.createElement("img")
     gif.src = excercise.gif
     gif.classList.add("excercise-gif")
-
     let musclesWorked = document.createElement("p")
     musclesWorked.textContent = excercise.musclesWorked
-
     let description = document.createElement("p")
     description.textContent = excercise.description
-    
     infoBox.append(name, gif, musclesWorked, description)
     document.getElementById("exercisesStack").append(infoBox)
 
